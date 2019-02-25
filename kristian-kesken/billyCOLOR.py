d5415d37ca6673aae6d517a400b164de894f49a8
kristiansyrjanen <35492474+kristiansyrjanen@users.noreply.github.com>
2019-02-25 11:54:12 +0200

    comments

--- kristian-kesken/billyCOLOR.py
+++ kristian-kesken/billyCOLOR.py
@@ -5,20 +5,20 @@ import argparse
 import cv2
 
 # parse args
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", help = "path to the image")
-#args = vars(ap.parse_args())
+"""ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())"""
 
 #list of boundaries
-#boundaries = [
-#    ([255,255,0], [0,0,255]), # Redball    boundaries 1 point
-#    ([0,0,0], [0,0,0]),       # Yellowball boundaries 2 points
-#    ([0,0,0], [0,0,0]),       # Greenball  boundaries 3 points
-#    ([0,0,0], [0,0,0]),       # Brownball  boundaries 4 points
-#    ([0,0,0], [0,0,0]),       # Blueball   boundaries 5 points
-#    ([0,0,0], [0,0,0]),       # Pinkball   boundaries 6 points
-#    ([0,0,0], [0,0,0])        # Blackball  boundaries 7 points
-#]
+"""boundaries = [
+    ([255,255,0], [0,0,255]), # Redball    boundaries 1 point
+    ([0,0,0], [0,0,0]),       # Yellowball boundaries 2 points
+    ([0,0,0], [0,0,0]),       # Greenball  boundaries 3 points
+    ([0,0,0], [0,0,0]),       # Brownball  boundaries 4 points
+    ([0,0,0], [0,0,0]),       # Blueball   boundaries 5 points
+    ([0,0,0], [0,0,0]),       # Pinkball   boundaries 6 points
+    ([0,0,0], [0,0,0])        # Blackball  boundaries 7 points
+]"""
 
 cap = cv2.VideoCapture('/home/kristian/topview-snooker.mp4')
 ret, current_frame = cap.read()
