d0ce06617c874b4a8e76676644b8c481210fb2ec
Matias <matias.richterich@myy.haaga-helia.fi>
2019-03-23 15:30:01 +0200

    program for videos detection

--- matias/tree/roi.py
+++ matias/tree/roi.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('pussi2.png',cv2.IMREAD_COLOR)
+img = cv2.imread('/home/matias/tree/pussi2.png',cv2.IMREAD_COLOR)
 
-px = img[55,55]
-img[55,55] = [255,255,255]
-px = img[55,55]
-print(px)
+#img[55,55] = [255,255,255]
+#px = img[55,55]
+
+img[100:150, 100:150] [20,20,1]
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
